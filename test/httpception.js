64284a6c15900e8920ac89983528e7647550e3d1
Andreas Lind <andreas@one.com>
2017-03-21 22:00:08 +0100

    Allow queueing up HTTP traffic before eventually running a promise factory.

--- test/httpception.js
+++ test/httpception.js
@@ -11,6 +11,10 @@ describe('httpception', function () {
             }, () => expect('GET http://example.com/', 'to yield response', 200));
         });
 
+        it('should succeed when a single function is passed and it does not perform any HTTP requests', function () {
+            return httpception(() => Promise.resolve());
+        });
+
         it('should fail when no HTTP requests are made and there is a one mocked out', function () {
             return expect(
                 httpception({
@@ -41,6 +45,67 @@ describe('httpception', function () {
                     '//\n' +
                     '// HTTP/1.1 200 OK'
             );
+        });
+
+        describe('when queueing up mock traffic before the promise factory is invoked', function () {
+            it('should succeed', function () {
+                httpception({
+                    request: 'GET http://example.com/',
+                    response: 200
+                });
+
+                return httpception([], () => expect('GET http://example.com/', 'to yield response', 200));
+            });
+
+            it('should succeed when queueing up twice', function () {
+                httpception({
+                    request: 'GET http://example.com/',
+                    response: 200
+                });
+
+                httpception({
+                    request: 'POST http://example.com/',
+                    response: 200
+                });
+
+                return httpception(
+                    [],
+                    () => expect('GET http://example.com/', 'to yield response', 200)
+                        .then(() => expect('POST http://example.com/', 'to yield response', 200))
+                );
+            });
+
+            it('should succeed when additional traffic is passed to the httpception call that launches the promise factory', function () {
+                httpception({
+                    request: 'GET http://example.com/',
+                    response: 200
+                });
+
+                return httpception({
+                    request: 'POST http://example.com/',
+                    response: 200
+                }, () => expect('GET http://example.com/', 'to yield response', 200)
+                    .then(() => expect('POST http://example.com/', 'to yield response', 200))
+                );
+            });
+
+            it('should fail with a diff', function () {
+                httpception({
+                    request: 'POST http://example.com/',
+                    response: 200
+                });
+                return expect(
+                    httpception([], () => expect('GET http://example.com/', 'to yield response', 200)),
+                    'to be rejected with',
+                        'GET / HTTP/1.1 // should be POST /\n' +
+                        '               //\n' +
+                        '               // -GET / HTTP/1.1\n' +
+                        '               // +POST / HTTP/1.1\n' +
+                        'Host: example.com\n' +
+                        '\n' +
+                        'HTTP/1.1 200 OK'
+                );
+            });
         });
     });
 });
