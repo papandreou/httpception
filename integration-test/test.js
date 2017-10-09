1eecbc2175e1b5a0bf4ff242e5f4fe2739a6eda5
Gustav Nikolaj Olsen <gno@one.com>
2017-10-09 12:16:55 +0200

    Run tests against jest as well as mocha
    The tests that spawn a test runner themselves have been separated out
    into the integration tests file and will not be run by both runners.

--- integration-test/test.js
+++ integration-test/test.js
@@ -13,18 +13,29 @@ describe('in afterEach mode', function () {
         "var httpception = require('../../');\n" +
         "var expect = require('unexpected').use(require('unexpected-http'));\n";
 
-    expect.addAssertion('<function> when run through mocha <assertion>', (expect, subject) => {
+    expect.addAssertion('<function> when run through (mocha|jest) <assertion>', (expect, subject) => {
         expect.errorMode = 'nested';
+        const isMocha = expect.alternations[0] === 'mocha';
         const code = subject.toString().replace(/^[^{]+\{|\}\s*$/g, '');
         expect.subjectOutput = function (output) {
             output.code(code, 'javascript');
         };
         const tmpFileName = pathModule.resolve(tmpDir, 'httpception' + Math.round(10000000 * Math.random()) + '.js');
-        const testCommand = process.argv[0] + ' ' + pathModule.resolve(__dirname, '..', 'node_modules', '.bin', 'mocha') + ' ' + tmpFileName;
+        var testCommand;
+        if (isMocha) {
+            testCommand = process.argv[0] + ' ' + pathModule.resolve(__dirname, '..', 'node_modules', '.bin', 'mocha') + ' ' + tmpFileName;
+        } else {
+            // jest
+            testCommand =
+                process.argv[0] + ' ' +
+                pathModule.resolve(__dirname, '..', 'node_modules', '.bin', 'jest') +
+                ' --config ' + pathModule.resolve(__dirname, 'jest.config.js') +
+                ' ' + tmpFileName;
+        }
 
         return fs.writeFileAsync(tmpFileName, preamble + code, 'utf-8')
         .then(() => expect.promise.fromNode(cb => childProcess.exec(testCommand, cb.bind(null, null))))
-        .then(stdoutAndStderr => expect.shift(stdoutAndStderr[1]))
+        .then((output) => expect.shift({ stdout: output[1], stderr: output[2] }))
         .finally(() => fs.unlinkAsync(tmpFileName));
     });
 
@@ -36,7 +47,11 @@ describe('in afterEach mode', function () {
                 return expect('GET /', 'to yield response', 200);
             });
             /* eslint-enable */
-        }, 'when run through mocha', expect.it('to contain', '✓ should foo').and('to contain', '1 passing'));
+        }, 'when run through mocha to satisfy', {
+            stdout: expect.it('to contain', '✓ should foo').and('to contain', '1 passing')
+        }).and('when run through jest to satisfy', {
+            stderr: expect.it('to contain', '✓ should foo').and('to contain', '1 passed, 1 total')
+        });
     });
 
     it('should fail with a diff when too few requests are made', function () {
@@ -46,7 +61,11 @@ describe('in afterEach mode', function () {
                 httpception({ request: 'GET /', response: 200 });
             });
             /* eslint-enable */
-        }, 'when run through mocha to match', /"after each" hook for "should foo"[\s\S]*\/\/ missing:\n\/\/ GET \/\n/);
+        }, 'when run through mocha to satisfy', {
+            stdout: /"after each" hook for "should foo"[\s\S]*\/\/ missing:\n\/\/ GET \/\n/
+        }).and('when run through jest to satisfy', {
+            stderr: expect.it('to contain', '✕ should foo').and('to contain', '1 failed, 1 total')
+        });
     });
 
     it('should fail with a diff when a request does not match the mocked out traffic', function () {
@@ -57,7 +76,9 @@ describe('in afterEach mode', function () {
                 return expect('/bar', 'to yield response', 200);
             });
             /* eslint-enable */
-        }, 'when run through mocha to contain',
+        }, 'when run through mocha to satisfy', {
+            stdout: expect.it(
+                'to contain',
                 'GET /bar HTTP/1.1 // should be GET /foo\n' +
                 '                  //\n' +
                 '                  // -GET /bar HTTP/1.1\n' +
@@ -65,7 +86,19 @@ describe('in afterEach mode', function () {
                 'Host: localhost\n' +
                 '\n' +
                 'HTTP/1.1 200 OK\n'
-        );
+            )
+        }).and('when run through jest to satisfy', {
+            stderr: expect.it(
+                'to contain',
+                '    GET /bar HTTP/1.1 // should be GET /foo\n' +
+                '                      //\n' +
+                '                      // -GET /bar HTTP/1.1\n' +
+                '                      // +GET /foo HTTP/1.1\n' +
+                '    Host: localhost\n' +
+                '    \n' +
+                '    HTTP/1.1 200 OK\n'
+            ).and('to contain', '1 failed, 1 total')
+        });
     });
 
     it('should fail with a diff the first test out of two fails', function () {
@@ -81,7 +114,9 @@ describe('in afterEach mode', function () {
                 return expect('GET /bar', 'to yield response', 200);;
             });
             /* eslint-enable */
-        }, 'when run through mocha to contain',
+        }, 'when run through mocha to satisfy', {
+            stdout: expect.it(
+                'to contain',
                 'GET /bar HTTP/1.1 // should be GET /foo\n' +
                 '                  //\n' +
                 '                  // -GET /bar HTTP/1.1\n' +
@@ -89,7 +124,19 @@ describe('in afterEach mode', function () {
                 'Host: localhost\n' +
                 '\n' +
                 'HTTP/1.1 200 OK\n'
-        );
+            )
+        }).and('when run through jest to satisfy', {
+            stderr: expect.it(
+                'to contain',
+                '    GET /bar HTTP/1.1 // should be GET /foo\n' +
+                '                      //\n' +
+                '                      // -GET /bar HTTP/1.1\n' +
+                '                      // +GET /foo HTTP/1.1\n' +
+                '    Host: localhost\n' +
+                '    \n' +
+                '    HTTP/1.1 200 OK\n'
+            ).and('to contain', '1 failed, 1 total')
+        });
     });
 
     it('should fail with a diff the second test out of two fails', function () {
@@ -105,7 +152,9 @@ describe('in afterEach mode', function () {
                 return expect('/bar', 'to yield response', 200);
             });
             /* eslint-enable */
-        }, 'when run through mocha to contain',
+        }, 'when run through mocha to satisfy', {
+            stdout: expect.it(
+                'to contain',
                 'GET /bar HTTP/1.1 // should be GET /foo\n' +
                 '                  //\n' +
                 '                  // -GET /bar HTTP/1.1\n' +
@@ -113,6 +162,52 @@ describe('in afterEach mode', function () {
                 'Host: localhost\n' +
                 '\n' +
                 'HTTP/1.1 200 OK\n'
-        );
+            )
+        }).and('when run through jest to satisfy', {
+            stderr: expect.it(
+                'to contain',
+                '    GET /bar HTTP/1.1 // should be GET /foo\n' +
+                '                      //\n' +
+                '                      // -GET /bar HTTP/1.1\n' +
+                '                      // +GET /foo HTTP/1.1\n' +
+                '    Host: localhost\n' +
+                '    \n' +
+                '    HTTP/1.1 200 OK\n'
+            ).and('to contain', '1 failed, 1 total')
+        });
+    });
+
+    it('should error with more than one req', function () {
+        return expect(() => {
+            /* eslint-disable */
+            it('should foo', function () {
+                httpception([
+                    { request: 'GET /foo', response: 200 },
+                    { request: 'GET /bar', response: 200 },
+                    { request: 'GET /baz', response: 200 }
+                ]);
+
+                return expect('/foo', 'to yield response', 200).then(
+                    () => expect('/foo', 'to yield response', 200)
+                );
+            });
+            /* eslint-enable */
+        }, 'when run through mocha to satisfy', {
+            stdout: expect.it(
+                'to contain',
+                'GET /foo HTTP/1.1 // should be GET /bar\n' +
+                '                  //\n' +
+                '                  // -GET /foo HTTP/1.1\n' +
+                '                  // +GET /bar HTTP/1.1'
+            )
+        }).and('when run through jest to satisfy', {
+            stderr: expect.it(
+                'to contain',
+                '    GET /foo HTTP/1.1 // should be GET /bar\n' +
+                '                      //\n' +
+                '                      // -GET /foo HTTP/1.1\n' +
+                '                      // +GET /bar HTTP/1.1'
+            ).and('to contain', '1 failed, 1 total')
+        });
     });
 });
