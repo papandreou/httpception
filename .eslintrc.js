af44177c935c6742812415dd7965f77363942550
Andreas Lind <andreaslindpetersen@gmail.com>
2018-05-27 21:56:35 +0200

    Add prettier setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,15 @@
-module.exports = {
-    extends: [
-        'onelint'
-    ],
-    env: {
-        es6: true
-    }
+const config = {
+  extends: ['pretty-standard'],
+  plugins: [],
+  rules: {}
 };
+
+if (process.stdin.isTTY) {
+  // Enable plugin-prettier when running in a terminal. Allows us to have
+  // eslint verify prettier formatting, while not being bothered by it in our
+  // editors.
+  config.plugins.push('prettier');
+  config.rules['prettier/prettier'] = 'error';
+}
+
+module.exports = config;
